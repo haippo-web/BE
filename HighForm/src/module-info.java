7f8f6288eac04130891eccee930e95673ec78a27
dziimin <jmncrk04@gmail.com>
2025-07-21 10:05:29 +0900

    feat : 관리자메뉴 강의 등록/수정/삭제 구현 - 관리자 메뉴에서 메뉴로 이동 일부만 구현(강의관리) - 강의 등록/수정/삭제 기능구현

--- HighForm/src/module-info.java
+++ HighForm/src/module-info.java
@@ -2,8 +2,11 @@ module HighForm {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires lombok;
+	requires java.sql;
 	
     opens com to javafx.graphics, javafx.fxml;
     opens com.board.controller to javafx.fxml;
     opens com.login.controller to javafx.fxml;
+    opens com.manager.controller to javafx.fxml; 
 }

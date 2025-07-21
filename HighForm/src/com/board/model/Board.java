30f66c96ba2a41e65aa1d12e2e9debf9d0128788
baejiwon <qowhxk@naver.com>
2025-07-22 08:28:19 +0900

    Board Entity 구현

---
+++ HighForm/src/com/board/model/Board.java
@@ -0,0 +1,26 @@
+package com.board.model;
+
+import com.common.BaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Board extends BaseEntity{
+	
+	private Long boardId;
+	private String title;
+	private String content;
+	private BoardCategory type;
+	private Long fileId;
+	
+	// TODO :: 유저 연동되면 매핑
+	private Long userId;
+}

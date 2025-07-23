4f6b9e0ef61402cf3c313c16a019f52cfc8356b4
snikuz12 <snikuz12@naver.com>
2025-07-23 11:36:32 +0900

    feat : calendar, todolist

--- HighForm/src/com/mypage/MyPageApp.java
+++ HighForm/src/com/mypage/MyPageApp.java
@@ -1,5 +1,7 @@
 package com.mypage;
 
+import com.mypage.controller.CalendarController;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,14 +10,19 @@ import javafx.stage.Stage;
 
 public class MyPageApp extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/mypage/TodoDialog.fxml"));
-        primaryStage.setTitle("My assignment List");
-        primaryStage.setScene(new Scene(root, 1000, 750));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/mypage/calendar.fxml"));
+        loader.setControllerFactory(type -> {                
+            if (type == CalendarController.class) {
+                return new CalendarController(1L);
+            }
+            try { return type.getDeclaredConstructor().newInstance(); }
+            catch (Exception e) { throw new RuntimeException(e); }
+        });
+        Scene scene = new Scene(loader.load(), 800, 600);
+        stage.setTitle("Calendar Test");
+        stage.setScene(scene);
+        stage.show();
     }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) { launch(args); }
 }
